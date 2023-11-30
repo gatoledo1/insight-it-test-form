b5389fb424a7320fd2530194652b05a9a5efbd24
Gabriel Toledo <gatoledo1@gmail.com>
2023-11-30 20:28:31 -0300

    Align Result

--- src/components/PrintData.js
+++ src/components/PrintData.js
@@ -8,24 +8,21 @@ const PrintData = ({ data }) => {
   };
 
   return (
-    <Container className="p-4 bg-light rounded-3">
+    <Container className="p-4 bg-light rounded-3 text-center">
       <Row>
         <h2 className="h2 text-secondary mb-3 text-center">Dados obtidos</h2>
       </Row>
       <Row>
-        <Col sm={6}>
+        <Col sm={6} lg={3}>
           <p>Nome: {data.nome}</p>
         </Col>
-        <Col sm={6}>
+        <Col sm={6} lg={3}>
           <p>Telefone: {data.telefone}</p>
         </Col>
-      </Row>
-      <Row>
-        <Col sm={6}>
+        <Col sm={6} lg={3}>
           <p>E-mail: {data.email}</p>
         </Col>
-
-        <Col sm={6}>
+        <Col sm={6} lg={3}>
           <p>Data de nascimento: {data.dataNascimento && formatDate(data.dataNascimento)}</p>
         </Col>
       </Row>
